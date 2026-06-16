1a83b2761dd2eee13ddd33d46f9ff33f1a60bda2
mutindarisper <mutindarisper@gmail.com>
2026-06-16 23:57:28 +0300

    create db, connect db to backend, use db with the API, ad migrations

--- main.py
+++ main.py
@@ -1,46 +1,30 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
 from pydantic import BaseModel
-  
+from sqlalchemy.orm import Session
+from database import Base, engine
+from deps import get_db
+from models import Course as CourseModel
+
 app = FastAPI()
+Base.metadata.create_all(bind=engine)
 
 class Item(BaseModel):
     name: str
     price: float
     is_offer: bool | None = None
 
-class Course(BaseModel):
-    id: int
+class CourseCreate(BaseModel):
     name: str
     description: str
-    lesson_count: int
 
+@app.post("/courses")
+def create_course(course: CourseCreate, db: Session = Depends(get_db)):
+    db_course = CourseModel(**course.model_dump())
+    db.add(db_course)
+    db.commit()
+    db.refresh(db_course)
+    return db_course
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
-
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: str | None = None):
-    return {"item_id": item_id, "q": q}
-
-@app.put("/items/{item_id}")
-def update_item(item_id: int, item: Item):
-    return {"item_name" : item.name, "item_price": item.price, "item_id": item_id}
-
-@app.get("/courses", response_model=list[Course])
-def read_courses():
-    return [
-        Course(
-            id=1,
-            name="Python",
-            description="Learn Python fundamentals.",
-            lesson_count=12
-        ),
-        Course(
-            id=2,
-            name="Javascript",
-            description="Learn JavaScript fundamentals.",
-            lesson_count=15
-        )
-    ]
+@app.get("/courses")
+def get_courses(db: Session = Depends(get_db)):
+    return db.query(CourseModel).all()
